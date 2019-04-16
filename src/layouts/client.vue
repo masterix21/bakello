a2a2c1be8ae65ba1e4dea728ec5aa842de8069a5
Danila Salerno <d.salerno@ambita.it>
2019-04-16 17:22:25 +0200

    personalizzazione menu

--- src/layouts/client.vue
+++ src/layouts/client.vue
@@ -6,37 +6,36 @@
             </div>
 
             <slot name="menu-left">
-              <ul class="p-5 list-reset text-grey-dark">
-                  <li class="text-xs">
-                      Anagrafiche
-
-                      <ul class="list-reset text-sm text-grey">
-                          <li><i class="fas fa-users text-xs mr-1"></i> Clienti</li>
-                          <li><i class="fas fa-tags text-xs mr-1"></i> Prodotti</li>
+              <ul class="list-reset text-grey-dark pt-5 pb-5 leading-loose">
+                  <li class="text-sm">
+                      <span class="font-bold text-white uppercase border-b border-grey-darker pt-3 pb-3 pl-5 pr-5 flex">Anagrafiche</span>
+                      <ul class="list-reset text-sm text-grey bg-grey-darker border-b border-grey-dark pt-2 pb-2 pl-5 pr-5 hidden">
+                          <li><i class="fas fa-users text-xs mr-2"></i> Clienti</li>
+                          <li><i class="fas fa-tags text-xs mr-2"></i> Prodotti</li>
                       </ul>
                   </li>
-                  <li class="pt-4 text-xs">
-                      Vendita
+                  <li class="text-sm">
+                      <span class="font-bold text-white uppercase bg-black border-b border-grey-darker pt-3 pb-3 pl-5 pr-5 flex">Vendita</span>
 
-                      <ul class="list-reset text-sm text-grey">
-                          <li><i class="fas fa-fire text-xs mr-1"></i> Preventivi</li>
-                          <li><i class="fas fa-piggy-bank text-xs mr-1"></i> Ordini</li>
+                      <ul class="list-reset text-sm text-grey bg-grey-darker border-b border-grey-dark pt-2 pb-2 pl-5 pr-5">
+                          <li><i class="fas fa-fire text-xs mr-2"></i> Preventivi</li>
+                          <li><i class="fas fa-piggy-bank text-xs mr-2"></i> Ordini</li>
                       </ul>
                   </li>
-                  <li class="pt-4 text-xs">
-                      Contabilit&agrave;
+                  <li class="text-sm">
+                      <span class="font-bold text-white uppercase border-b border-grey-darker pt-3 pb-3 pl-5 pr-5 flex">Contabilit&agrave;</span>
 
-                      <ul class="list-reset text-sm text-grey">
-                          <li><i class="fas fa-euro-sign text-xs mr-1"></i> Fatture</li>
-                          <li><i class="fab fa-creative-commons-nc-eu text-xs mr-1"></i> Note credito</li>
+                      <ul class="list-reset text-sm text-grey bg-grey-darker border-b border-grey-dark pt-2 pb-2 pl-5 pr-5 hidden">
+                          <li><i class="fas fa-euro-sign text-xs mr-2"></i> Fatture</li>
+                          <li><i class="fab fa-creative-commons-nc-eu text-xs mr-2"></i> Note credito</li>
                       </ul>
                   </li>
-                  <li class="pt-4 text-xs">
-                      Magazzino
+                  <li class="text-sm">
+                      <span class="font-bold text-white uppercase border-b border-grey-darker pt-3 pb-3 pl-5 pr-5 flex">Magazzino</span>
 
-                      <ul class="list-reset text-sm text-grey">
-                          <li><i class="fas fa-people-carry text-xs mr-1"></i> DDT</li>
-                          <li><i class="fas fa-warehouse text-xs mr-1"></i> Giacenze</li>
+                      <ul class="list-reset text-sm text-grey bg-grey-darker border-b border-grey-dark pt-2 pb-2 pl-5 pr-5 hidden">
+                          <li><i class="fas fa-people-carry text-xs mr-2"></i> DDT</li>
+                          <li><i class="fas fa-warehouse text-xs mr-2"></i> Giacenze</li>
                       </ul>
                   </li>
               </ul>
@@ -62,6 +61,7 @@
     </div>
 </template>
 
+
 <script>
 export default {
   name: 'bklo-layouts-client',
@@ -73,6 +73,8 @@ export default {
   }
 }
 </script>
+
+
 
 <style scoped>
 
