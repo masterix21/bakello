6c1ff8a101c926fca056fd96a214dd2bc6ddabd4
Luca Longo <l.longo@ambita.it>
2019-04-19 16:09:48 +0200

    Client sidebar menu clickable on mobile

--- src/layouts/client.vue
+++ src/layouts/client.vue
@@ -4,7 +4,10 @@
             <div class="w-16 sm:w-16 md:w-48 bg-black items-center justify-center text-grey shadow border-b-4 border-grey-darker">
                 <slot name="header-logo">
                     <div class="text-center text-xs p-2 sm:p-5 sm:text-sm md:text-base">Logo</div>
-                    <div class="sm:hidden text-center text-2xl"><i class="fas fa-bars"></i></div>
+
+                    <a @click="sidebarOpened = !sidebarOpened;" class="block sm:hidden text-center text-2xl">
+                        <i class="fas fa-bars"></i>
+                    </a>
                 </slot>
             </div>
             <div class="flex-1 bg-grey-light">
@@ -21,7 +24,7 @@
         </div>
 
         <div class="flex w-screen bg-grey flex-grow">
-            <div :class="['hidden sm:block sm:w-16 md:w-48', sidebarClasses]">
+            <div :class="['sm:block sm:w-16 md:w-48', ...sidebarClasses, {'hidden': !sidebarOpened, 'absolute mb-0': sidebarOpened}]">
                 <slot name="menu-left"></slot>
             </div>
             <div class="flex-1 bg-grey-lighter w-screen p-8">
@@ -40,10 +43,12 @@ export default {
     props: {
         sidebarClasses: {
             type: Array,
-            default: () => ['bg-grey-darkest'],
+            default: () => (['bg-grey-darkest']),
         }
     },
-    data: () => ({ }),
+    data: () => ({
+        sidebarOpened: false,
+    }),
     computed: {
 
     },
