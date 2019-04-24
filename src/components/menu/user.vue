95be3a10cb533d191501ec9476055cc41df72742
Danila Salerno <d.salerno@ambita.it>
2019-04-24 12:20:34 +0200

    update user submenu
    - avatar position in xs view managed

--- src/components/menu/user.vue
+++ src/components/menu/user.vue
@@ -1,7 +1,7 @@
 <template>
     <div :class="['h-full relative w-32 -ml-10 sm:w-auto', {'border-grey-light border-b-4': !opened, 'border-grey-dark border-l-4 border-b-4 bg-grey': opened}]">
         <a class="h-full flex items-center justify-center cursor-pointer px-6" @click="opened = !opened;">
-            <img :src="avatar" :class="['border-2 rounded-full align-middle sm:mr-2', {'border-grey-light': opened}]" />
+            <img :src="avatar" :class="['border-2 rounded-full align-middle -mr-10 sm:mr-2', {'border-grey-light -ml-10 sm:-ml-1': opened}]" />
             <p :class="['text-sm text-grey-dark hidden sm:block', {'text-grey-lighter': opened}]">{{displayName}}</p>
             <i :class="['text-xs ml-2 mt-1 text-grey-dark hidden sm:block', {'fas fa-times text-grey-lighter': opened}, {'fas fa-angle-down': !opened}]"></i>
         </a>
